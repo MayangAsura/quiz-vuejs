ea380d8a2ecbfe7554cbbaac3cefccc51cb50063
MayangAsura <mayang.asura123@gmail.com>
2020-11-15 14:23:31 +0700

    update v1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,12 +17,15 @@ var app = new Vue({
             this.users.push({name: this.name_input})
             this.name_input = ''
         },
-        edit: function(index){
+        edit: function(index=null){
             this.name_input = this.users[index].name
             this.add = false
         },
         hapus: function(index){
             this.users.splice(index, 1)
-        }
+        },
+        // update: function(){
+        //     this.users[index]
+        // }
     }
 })
